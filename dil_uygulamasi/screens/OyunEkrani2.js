acbfe2bd954ec7457d76055a96d5f91ae5b900ae
Nurihank <144276492+Nurihank@users.noreply.github.com>
2024-10-10 15:59:20 +0300

    oyunekranı

--- dil_uygulamasi/screens/OyunEkrani2.js
+++ dil_uygulamasi/screens/OyunEkrani2.js
@@ -1,21 +1,25 @@
-import { Alert, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, FlatList, Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import api from '../api/api';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Speech from 'expo-speech';
+import { Button, ProgressBar } from 'react-native-paper';
 
 export default function OyunEkrani2(props) {
     const [soru, setSoru] = useState(0);
     const [kelimeler, setKelimeler] = useState([]);
-    const [anaKelime, setAnaKelime] = useState(null);
+    const [anaKelime, setAnaKelime] = useState("");
     const [anakelimeler, setAnaKelimeler] = useState([]);
     const [devamEtButton, setDevamEtButton] = useState(false);
     const [yanlisKelimeler, setYanlisKelimeler] = useState([]);
+    const [dogruYüzdesi,setDogruYüzdesi] = useState(0);
     const [seciliSik, setSeciliSik] = useState(null);
     const [cevapDurumu, setCevapDurumu] = useState(null);
     const [userId, setUserId] = useState(null);
-
+    const [soruAlertModal, setSoruAlertModal] = useState(false);
+    const [oyunSonuAlertModal, setOyunSonuAlertModal] = useState(false);
+    const [bolumuGectinMi,setBolumuGectinMi] = useState(false)
     const navigation = useNavigation();
 
     const getUserID = async () => {
@@ -24,13 +28,12 @@ export default function OyunEkrani2(props) {
       };
 
     const DigerSoru = async (yanlisKelime) => {
-
         let yeniYanlisKelimeler = yanlisKelimeler;
         if (yanlisKelime) {
             yeniYanlisKelimeler = [...yanlisKelimeler, yanlisKelime];
             setYanlisKelimeler(yeniYanlisKelimeler);  // Yanlış kelimeleri güncelle
         }
-
+        setSoruAlertModal(false)
         if (soru >= 2) { 
             console.log(yeniYanlisKelimeler); // Yeni yanlış kelimeler listesini göster
             if(yeniYanlisKelimeler.length > 1){
@@ -94,17 +97,25 @@ export default function OyunEkrani2(props) {
         console.log(response.data.message)
         if(response.data.message == "succes"){
 
-            const sezonunBolumleriniGetir = await api.get("/kullanici/SezonBittiMiKontrol",{
+            const sezonunBittiMi = await api.get("/kullanici/SezonBittiMiKontrol",{
                 params:{
                     KullaniciID:userId,
                     SezonID:props.route.params.SezonID
                 }
             })
-            console.log("asd = "+sezonunBolumleriniGetir.data.sezonBittiMi) //burda kaldın true dönerse sezon ekle bu kaddars
-
+            if(sezonunBittiMi.data.sezonBittiMi){
+                const sezonEkle = await api.post("/kullanici/GecilenSezonEkle",{
+                    KullaniciID:userId,
+                    SezonID:props.route.params.SezonID
+                })
+                console.log("sezon ekleme işi = "+sezonEkle.data)
+                
+            }
+            console.log("sezon bitti mi  = "+sezonunBittiMi.data.sezonBittiMi)
 
             navigation.replace("HomeScreen");
         }
+        //önceden oynadıysa ve leveli  geçtiyse buraya girecek
         navigation.replace("HomeScreen")
     }
 
@@ -114,27 +125,6 @@ export default function OyunEkrani2(props) {
             console.log("Yanlış Kelimeler:", yanlisKelimeler); // Yanlış kelimeleri burada yazdırın
         }
     }, [yanlisKelimeler]);
-
-    const TekrarDene = ()=>{
-        setAnaKelime(anakelimeler[soru])
-        setDevamEtButton(false); 
-        setSeciliSik(null); 
-        setCevapDurumu(null); 
-        const data = kelimeler;
-
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];  
-            }
-            return array;
-        }
-
-        if (data.length > 0) {
-            const shuffledData = shuffleArray([...data]); 
-            setKelimeler(shuffledData);
-        }
-    }
 
     const KelimeleriGetir = async () => {
         try {
@@ -177,7 +167,10 @@ export default function OyunEkrani2(props) {
     }
 
     const CevapDogruMu = (cevap) => {
+        console.log(anaKelime)
         if (cevap === anaKelime.value) {
+            setDogruYüzdesi(dogruYüzdesi+33)
+            console.log(dogruYüzdesi)
             if (soru >= 2) { 
                 DigerSoru() 
             } else {
@@ -185,22 +178,7 @@ export default function OyunEkrani2(props) {
             }
             setCevapDurumu('correct'); 
         } else {
-            Alert.alert(
-                "Yanlış Cevap!",
-                "Lütfen cevabınızı kontrol edin.",
-                [
-                    {
-                        text: "Tekrar Dene", 
-                        onPress: () => TekrarDene(),
-                        style: "default"
-                    },
-                    {
-                        text: "Devam Et", 
-                        onPress: () => DigerSoru(anaKelime.value),
-                        style: "default"
-                    }
-                ]
-            );
+            setSoruAlertModal(true)
             setCevapDurumu('incorrect'); 
         }
         setSeciliSik(cevap); 
@@ -247,27 +225,28 @@ export default function OyunEkrani2(props) {
                 ]}
             >
                 <Text style={styles.answerText}>{item.ceviri}</Text>
-                
             </TouchableOpacity>
         );
     };
 
     return ( 
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.questionCount}>{soru + 1}/3</Text>
-                <TouchableOpacity onPress={() => SozlugeEkle(anaKelime)}>
-                    <Text style={styles.title}>Sözlüğe Ekle</Text>
-                </TouchableOpacity>
+            <View style={styles.progressBarContainer}>
+                <ProgressBar progress={dogruYüzdesi/100} style={styles.progressBar} />
+                <View style={styles.progressMarker} />
             </View>
+    
             <View style={styles.askContainer}>
                 <Image source={require("../assets/question.png")} style={styles.image} />  
                 {anaKelime && <Text style={styles.askText}>{anaKelime.value}</Text>}
                 <TouchableOpacity onPress={()=>speakWord(anaKelime.value)}>
                     <Image source={require("../assets/microphone.png")} style={{height:40,width:40}}/>
-
+                </TouchableOpacity>
+                    <TouchableOpacity onPress={() => SozlugeEkle(anaKelime)} style={styles.sozlukButton}>
+                    <Text style={styles.title}>Sözlüğe Ekle</Text>
                 </TouchableOpacity>
             </View>
+    
             <FlatList
                 data={kelimeler}
                 renderItem={renderItem}
@@ -275,16 +254,44 @@ export default function OyunEkrani2(props) {
                 contentContainerStyle={styles.answerContainer}
                 numColumns={2}
             />
+            
             {devamEtButton 
-                ?
-                <TouchableOpacity onPress={() => DigerSoru()}>
+                ? <TouchableOpacity onPress={() => DigerSoru()}>
                     <Image source={require("../assets/nextButton.png")} style={styles.nextButton}/>
-                </TouchableOpacity>
-                : 
-                null
+                  </TouchableOpacity>
+                : null
             }
+            <Modal
+                visible={soruAlertModal}
+                transparent={true}
+                animationType="slide"
+            >
+                <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                <View>
+                    <Text>
+                        Yanlış Cevap
+                    </Text>
+                    <Text>
+                        {anaKelime.value}   kelimesi    {anaKelime.ceviri} demek
+
+                    </Text>
+                </View>                      
+                    <View style={styles.alertButtonGroup}>
+                    <TouchableOpacity style={styles.alertButton} onPress={() => DigerSoru(anaKelime.value)}>
+                        <Text style={styles.buttonText}>Devam Et</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.alertButton} onPress={() => SozlugeEkle(anaKelime)}>
+                        <Text style={styles.buttonText}>Sözlüğe Ekle</Text>
+                    </TouchableOpacity>
+                    
+                    </View>
+                </View>
+                </View>
+            </Modal>
         </View>
     );
+    
 }
 
 const styles = StyleSheet.create({
@@ -293,21 +300,22 @@ const styles = StyleSheet.create({
         backgroundColor: "#f5f5f5", // Light Gray
         padding: 20,
     },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20,
+    progressBarContainer:{
+        marginTop:35
     },
-    questionCount: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#0288d1', // Darker Blue
+    progressMarker: {
+        position: 'absolute',
+        top: 0,
+        left: '65%', // Progresyon yüzde 35'te olduğu için işareti buraya yerleştiriyoruz
+        width: 5, // İşaretin genişliği
+        height: '55%', // İşaretin barın yüksekliğine eşit olması için
+        backgroundColor: 'red', // İşaretin rengi
+        borderRadius:100
     },
     title: {
         fontSize: 18,
         fontWeight: 'bold',
-        color: '#0288d1', // Darker Blue
+        color: 'red', // Darker Blue
         textTransform: 'uppercase',
     },
     askContainer: {
@@ -322,7 +330,8 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 10,
         elevation: 8,
-        flexDirection: "row"
+        flexDirection: "row",
+        position: 'relative', // Bu gerekli, çünkü sözlük butonunu sağ üstte konumlandıracağız
     },
     image: {
         height: 120,
@@ -336,6 +345,14 @@ const styles = StyleSheet.create({
         color: "#333333", // Dark Gray
         textAlign: 'center',
         marginHorizontal: 10,
+    },
+    sozlukButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        backgroundColor: '#0288d1', // Blue background for button
+        padding: 10,
+        borderRadius: 8,
     },
     answerContainer: {
         marginTop: 30,
@@ -373,6 +390,46 @@ const styles = StyleSheet.create({
         width: 70,
         alignSelf: 'flex-end',
         marginTop: 20,
-    }
+    },
+    progressBar: {
+        width: '100%',
+        height: 25, // Barın yüksekliği
+        borderRadius: 10, // Barın köşelerini yuvarlatmak için
+        borderWidth: 2, // Çizgi kalınlığı
+        borderColor: '#0288d1', // Çizgi rengi
+        marginBottom: 20, // ProgressBar ile askContainer arası boşluk
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+      },
+      modalContent: {
+        width: '80%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 8,
+      },
+      alertButtonGroup: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      },
+      alertButton: {
+        marginTop: 12,
+        padding: 10,
+        backgroundColor: '#0288d1', // Buton arka plan rengi
+        borderRadius: 8,
+        alignItems: 'center',
+      },
+      buttonText: {
+        color: 'white', // Yazı rengi
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
 });
-
